fad807886834257aa5bc297ce13193759818a601
katiakate <ekaterina@imac.local>
2023-07-19 15:57:10 +0300

    Added users' routes

--- backend/api/views.py
+++ backend/api/views.py
@@ -21,7 +21,53 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
+    @action(
+        detail=False,
+        methods=('post',),
+        serializer_class=...,
+    )
+    def set_password(self, request):
+        ...
+
+    @action(
+        detail=False,
+        serializer_class=...,
+    )
+    def subscriptions(self, request):
+        ...
+
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        serializer_class=...,
+    )
+    def subscribe(self, request):
+        ...
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     http_method_names = ('get', 'post', 'patch', 'delete')
     ...
+
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        serializer_class=...,
+    )
+    def favorite(self, request):
+        ...
+
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        serializer_class=...,
+    )
+    def shopping_cart(self, request):
+        ...
+
+    @action(
+        detail=False,
+        serializer_class=...,
+    )
+    def download_shopping_cart(self, request):
+        ...
