276f4e94f3a62a5f738fde413637802013f9646d
katiakate77 <>
2023-07-27 23:13:44 +0300

    permissions divided into 2 methods

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,13 +3,17 @@ from rest_framework import permissions
 
 class AccessOrReadOnly(permissions.BasePermission):
 
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        if request.method == 'POST':
-            return request.user.is_authenticated
+        if request.user == obj.author:
+            return True
 
-        return (
-            request.user.is_authenticated and request.user == obj.author
-        )
+        return False
