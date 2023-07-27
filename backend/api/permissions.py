aba97c244b7b7a5c5c59b3eb2210cf71f31d7ddc
katiakate77 <>
2023-07-27 23:34:17 +0300

    permission update

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,16 +4,15 @@ from rest_framework import permissions
 class AccessOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
 
-        return request.user.is_authenticated
+        return (
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
 
-        if request.user == obj.author:
-            return True
-
-        return False
+        return (
+            request.user == obj.author
+            or request.method in permissions.SAFE_METHODS
+        )
