499da7e53b34faec31b13e899faf42717208551f
Ekaterina Petrova <43879306+katiakate77@users.noreply.github.com>
2024-02-06 20:54:08 +0300

    Added database comment

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -87,9 +87,6 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
